091b3371474ba01bca835837cdc75102c01c53db
Karamyshev <karamyshev.v@gmail.com>
2022-12-12 14:23:41 +0200

    HW3 done

---
+++ HW-03/main.py
@@ -0,0 +1,28 @@
+#Task 1
+a = "I love Python"
+b = a * 42
+print(b)
+
+#Task 2
+age_in_month = 33 * 12
+
+#Task 3
+age_in_years = int(age_in_month / 12)
+
+#Task 4
+my_age = "Му name is Valerii I’m " + str(age_in_years) + " years old"
+
+#Task 5
+a = 158
+print(a >= 300)
+print(a <= 200)
+print(a != 158)
+print(a == 160)
+print(a > 30)
+
+#Task 6
+a=2
+b=5
+c=6
+d = str(a) + str(b) + str(c)
+
